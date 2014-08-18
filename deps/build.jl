256bf1aa8036f3154b95e0a1bb556525431eff4b
Keno Fischer <kfischer@college.harvard.edu>
2014-08-18 17:01:35 -0400

    Small build.jl fix

--- deps/build.jl
+++ deps/build.jl
@@ -13,6 +13,11 @@ gvc = library_dependency("gvc",aliases = ["libgvc"])
 
 graphviz = [cgraph,gvc]
 
+@osx_only begin
+    using Homebrew
+    provides( Homebrew.HB, "graphviz", graphviz, os = :Darwin )
+end
+
 options = String[]
 x11 ? push!(options,"--with-x") : push!(options,"--without-x")
 gtk && push!(options,"--with-gtk")
@@ -21,7 +26,8 @@ push!(options,"--with-pangocairo")
 push!(options,"--enable-debug")
 
 provides(Sources,URI("http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.36.0.tar.gz"),graphviz)
-provides(BuildProcess,Autotools(libtarget = "lib/cgraph/.libs/libcgraph."*BinDeps.shlib_ext,configure_options=options),graphviz)
+provides(BuildProcess,Autotools(libtarget = "lib/cgraph/.libs/libcgraph."*BinDeps.shlib_ext,configure_options=options,
+    pkg_config_dirs=[Pkg.dir("Cairo","deps","usr","lib","pkgconfig")]),graphviz)
 
 # Ubuntu GraphViz is too old
 # provides(AptGet,"graphviz",graphviz)
