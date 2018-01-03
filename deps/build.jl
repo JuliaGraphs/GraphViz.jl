b25bb1a3320dbda5c39bfd25d042e625980cd963
ysaito8015 <ysaito8015@gmail.com>
2018-01-03 19:38:43 +0900

    Update build.jl
    fix source download url.

--- deps/build.jl
+++ deps/build.jl
@@ -33,7 +33,7 @@ push!(options,"--without-qt")
 push!(options,"--with-pangocairo")
 push!(options,"--enable-debug")
 
-provides(Sources,URI("http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.36.0.tar.gz"),graphviz)
+provides(Sources,URI("https://graphviz.gitlab.io/pub/graphviz/stable/SOURCES/graphviz.tar.gz"),graphviz)
 provides(BuildProcess,Autotools(libtarget = "lib/cgraph/.libs/libcgraph."*BinDeps.shlib_ext,configure_options=options,
     pkg_config_dirs=[Pkg.dir("Cairo","deps","usr","lib","pkgconfig")]),graphviz)
 
